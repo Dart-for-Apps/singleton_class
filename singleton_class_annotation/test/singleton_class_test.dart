ed75c611fd1fa6d36857762e829edf69b32b5d79
wurikiji <wurikiji@gmail.com>
2019-10-27 03:06:34 -0700

    Preparation for publish

--- singleton_class_annotation/test/singleton_class_test.dart
+++ singleton_class_annotation/test/singleton_class_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:singleton_class/singleton_class.dart';
+import 'package:singleton_class_annotation/singleton_class_annotation.dart';
 
 void main() {
   test('adds one to input values', () {
--- singleton_class_generator/test/singleton_class_generator_test.dart
+++ singleton_class_generator/test/singleton_class_generator_test.dart
@@ -3,11 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:singleton_class_generator/singleton_class_generator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  test('adds one to input values', () {});
 }
